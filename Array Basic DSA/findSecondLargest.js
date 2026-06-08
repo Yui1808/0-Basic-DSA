efe81ee95894b8d7a32b3ab6b3e325a500f943df
Yui1808 <anshulchak49@gmail.com>
2026-06-08 09:05:14 +0530

    complexity kam kari

--- Array Basic DSA/findSecondLargest.js
+++ Array Basic DSA/findSecondLargest.js
@@ -8,8 +8,12 @@ let largest = -Infinity;
 let secondLargest = -Infinity;
 for(let i = 0;i<arr.length;i++){
     if(arr[i]>largest){
-        secondLargest = largest
-        largest = arr[i]
+        secondLargest = largest;
+        largest = arr[i];
     }
+
+    else if(secondLargest<arr[i] && arr[i]<largest){
+            secondLargest = arr[i];
+        }
 }
-console.log(largest)
+console.log(secondLargest);
