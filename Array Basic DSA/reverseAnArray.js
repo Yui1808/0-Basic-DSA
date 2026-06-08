8083bc571227eaede588a72250425c8b80def0d2
Yui1808 <anshulchak49@gmail.com>
2026-06-08 08:38:45 +0530

    while vala version

--- Array Basic DSA/reverseAnArray.js
+++ Array Basic DSA/reverseAnArray.js
@@ -3,16 +3,16 @@ Yeh sawal tera Two-Pointer Approach aur Swapping (adla-badli) ka darr hamesha ke
 
 Problem: Ek array hai [1, 2, 3, 4, 5]. Tujhe isko ulta karke [5, 4, 3, 2, 1] banana hai.*/
 let arr = [1, 2, 3, 4, 5];
-let holder ;
-let start =0;
-let end = arr.length-1;
-for(start =0;start<end;){
+let holder;
+let start = 0;
+let end = arr.length - 1;
+while (start < end) {
     holder = arr[end];
     arr[end] = arr[start];
-    arr[start] =holder;
+    arr[start] = holder;
     start++;
-    end--
-    
+    end--;
+
 }
 console.log(arr)
 
