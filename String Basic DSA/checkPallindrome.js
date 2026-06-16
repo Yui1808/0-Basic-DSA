40bc091e816298dc16370a7cce6f15564ab50f18
Yui1808 <anshulchak49@gmail.com>
2026-06-16 15:06:09 +0530

    Bhai adha adhura

--- String Basic DSA/checkPallindrome.js
+++ String Basic DSA/checkPallindrome.js
@@ -1,23 +1,39 @@
-let string = "zoroz";
+let str = "a man, a plan, a canal: panama";
 let start = 0;
-let end = string.length-1;
+let end = str.length-1;
 let flag = true;
 
-while(start<=end){
-   if(string[start]!==string[end]){
-     flag = false;
-     break;
-   }
-   
+while(start<end){
 
-   start++
-   end--
+    if(str[start]==="," || str[start]===":" || str[start]===" " ){
+
+    start++
+
+    }
+
+
+    if(str[end ]==="," || str[end]===":" || str[end]===" " ){
+
+    end--;
+
+    }
+
+    
+    if(str[start]!==str[end]){
+        flag = false;
+        break;
+    }
+    
+    start++;
+    end--
+
 
 }
 
 if(flag){
-    console.log(`string ${string} is Pallindrome`);
+    console.log("yes")
 }
+
 else{
-    console.log(`string ${string} is not Pallindrome`);
+    console.log("no")
 }
