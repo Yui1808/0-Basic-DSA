e61d8d3e8982c1670ab098384652c90e6c301c4a
Yui1808 <anshulchak49@gmail.com>
2026-06-16 13:00:10 +0530

    bhai optimised version

--- Array Manipulation using Pointer Approach/reverseAnArray.js
+++ Array Manipulation using Pointer Approach/reverseAnArray.js
@@ -1,14 +1,12 @@
-// Reverse String / Array
+/*Interviewer ne tujhe ek array diya hai aur bola hai ki isko ulta karke do, bina kisi extra array ke (yaani $O(1)$ space mein).Maan le array yeh hai:let nums = [1, 2, 3, 4, 5];Tujhe isko badal kar banana hai: [5, 4, 3, 2, 1].*/
 
-let arr = [1,2,3,4,5];
+
+let arr = [1, 2, 3, 4, 5];
 let start = 0;
 let end = arr.length-1;
-while(start<=end){
-    arr[start] = arr[start] + arr[end] ;
-    arr[end] = arr[start] - arr[end];
-    arr[start] = arr[start] - arr[end];
+while(start<end){
+    [arr[start],arr[end]] = [arr[end],arr[start]];
     start++;
     end--;
 }
-
 console.log(arr)
